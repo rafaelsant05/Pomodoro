10ee2bb49107b05906d4b32e3c89b0b97a8e7ada
Rafael Carvalho <rafaell8.duss4nto5@gmail.com>
2026-05-15 20:43:30 -0300

    fix: remove pasta chronos-pomodoro duplicada

--- praticas/pomodoro/chronos-pomodoro/src/components/CountDown/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css';
-import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
-
-export function CountDown() {
-  const { state } = useTaskContext();
-  return (
-    <div className={styles.container}>{state.formattedSecondsRemaining}</div>
-  );
-}
